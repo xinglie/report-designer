46be9eda26d4200562019de33f5ba3d3faa96dcd
xinglie.lkf <xinglie.lkf@taobao.com>
2020-10-25 10:06:28 +0800

    code clean and bug fixes

--- sync.sh
+++ sync.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 git co iot dist/iot.js
-git co iot dist/display.js
+#git co iot dist/display.js
 git co iot dist/printer.js
 git co absolute dist/absolute.js
 git co page dist/page.js
 git co iot dist/mini.js
+git co iot index.html
+git co iot print.html
+git co iot mini.html
+git co iot apis/**
