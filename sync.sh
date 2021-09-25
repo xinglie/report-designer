ea024247e005a7ae9179ab214a6bd79773af39a5
xinglie.lkf <xinglie.lkf@taobao.com>
2021-09-25 10:36:01 +0800

    read changelog.md 2021-09-25

--- sync.sh
+++ sync.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
-git co iot dist/index.js
-#git co iot dist/display.js
+git co iot dist/designer.js
 git co iot dist/printer.js
-git co iot dist/standalone.js
-# git co absolute dist/absolute.js
-# git co page dist/page.js
-git co iot dist/mini.js
+
 git co iot index.html
 git co iot print.html
 git co iot mini.html
