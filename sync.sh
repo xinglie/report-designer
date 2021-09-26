166223bc83ac37567000ce8f54231e1ff19894f5
xinglie.lkf <xinglie.lkf@taobao.com>
2021-09-26 12:20:11 +0800

    read changelog.md 2021-09-26

--- sync.sh
+++ sync.sh
@@ -6,6 +6,7 @@ git co iot index.html
 git co iot print.html
 git co iot mini.html
 git co iot standalone.html
+git co iot split.html
 git co iot apis/**
 git co iot CHANGELOG.md
 git co iot images/**
