fa0a7228b9c839ec16ad3eddc21bbd61a8202362
xinglie.lkf <xinglie.lkf@taobao.com>
2022-08-25 18:53:41 +0800

    read changlog.md 2022-08-25

--- sync.sh
+++ sync.sh
@@ -3,7 +3,7 @@ git co iot dist/designer.js
 git co iot dist/viewer.js
 
 git co iot index.html
-git co iot print.html
+git co iot viewer.html
 git co iot mini.html
 git co iot standalone.html
 git co iot split.html
